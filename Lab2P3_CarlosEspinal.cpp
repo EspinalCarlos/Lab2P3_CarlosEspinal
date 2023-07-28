17d99dd8d202fcdff3a3465b5a34d288e736f6a0
Carlos Espinal <cespinalb@gmail.com>
2023-07-28 15:00:39 -0600

    Commit de las 3

--- Lab2P3_CarlosEspinal.cpp
+++ Lab2P3_CarlosEspinal.cpp
@@ -2,12 +2,64 @@
 using namespace std;
 
 void menu() {
-    cout << "---- MENU DE OPCIONES ----" << "Opcion 1: Tablero de Ajedrez" << "Opcion 2: Raiz n de x";
+    cout << "---- MENU DE OPCIONES ----\n" << "Opcion 1: Tablero de Ajedrez\n" << "Opcion 2: Raiz n de x\n" ;
+}
+
+void ejercicio1() {
+	int numtablero;
+	cout << "Ingrese un numero: ";
+	cin >> numtablero;
+
+	for (size_t i = 0; i < numtablero; i++){
+		if (i%2 == 0){
+			for (size_t i = 0; i < numtablero; i++){
+				if (i%2 == 0){
+					cout << "1";
+				}
+				else if (i%2 != 0){
+					cout << "0";
+				}
+			}
+			cout << endl;
+			
+		}
+		else {
+			for (size_t i = 0; i < numtablero; i++){
+				if (i%2 == 0){
+					cout << "0";
+				}
+				else if (i%2 != 0) {
+					cout << "1";
+				}
+			}
+			cout << endl;
+
+		}
+
+	}
+
 }
 
 
 int main(){
+	int opcion;
+	do{
+		menu();
+		cout << "Ingrese la opcion que desea utilizar: ";
+		cin >> opcion; 
+		cout << endl;
 
+		switch (opcion){
+			case 1:
+				ejercicio1();
+
+			default:
+				break;
+		}
+
+
+
+	} while (opcion != 3);
     
 }
 
