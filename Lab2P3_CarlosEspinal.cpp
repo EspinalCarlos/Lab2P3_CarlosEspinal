47f0e13c59915d2b93344cbb7f1b3e38a7a755e6
Carlos Espinal <cespinalb@gmail.com>
2023-07-28 13:50:15 -0600

    Primer commit

---
+++ Lab2P3_CarlosEspinal.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+void menu() {
+    cout << "---- MENU DE OPCIONES ----" << "Opcion 1: Tablero de Ajedrez" << "Opcion 2: Raiz n de x";
+}
+
+
+int main(){
+
+    
+}
+
